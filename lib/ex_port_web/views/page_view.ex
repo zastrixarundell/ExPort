6646c6802fc33d0c9154987d12d5c193e184b753
zastrixarundell <zastrixarundell@gmail.com>
2021-09-10 04:15:50 +0200

    Added basic looks

--- lib/ex_port_web/views/page_view.ex
+++ lib/ex_port_web/views/page_view.ex
@@ -1,3 +1,8 @@
 defmodule ExPortWeb.PageView do
   use ExPortWeb, :view
+
+  @spec years_old :: non_neg_integer()
+  def years_old do
+    Timex.diff(Date.utc_today(), Date.from_iso8601!(System.get_env("BIRTHDAY")), :years)
+  end
 end
